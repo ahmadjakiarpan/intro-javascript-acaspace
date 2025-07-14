7acfcbae08130c015de9b2e0bf3f06a80c5e4ace
ahmadjakiarpan <ahmadjakii462@gmail.com>
2025-07-15 00:38:04 +0700

    update task 1

--- task/task.js
+++ task/task.js
@@ -1,10 +1,12 @@
 
 // 1. Srtuktur data
 
+// object of array
 const samsungs22ultra = {
     title: "Samsung S22 Ultra",
     specification: "Camera 50M, Ram 12GB, Fast Charger.",
     price: 5000000,
+    discount: true,
     color: ["white", "black", "blue"],
 
 }
@@ -13,6 +15,7 @@ const samsunggalaxym13 = {
     title: "Samsung galaxy M13",
     specification: "Camera 20M, Ram 6GB, internal up to 1TB.",
     price: 2000000,
+    discount: true,
     color: ["white", "black", "blue"],
 }
 
@@ -20,6 +23,7 @@ const samsunggalaxym33 = {
     title: "Samsung galaxy M33",
     specification: "Camera 30M, Ram 8GB, internal up to 1TB.",
     price: 3000000,
+    discount: false,
     color: ["brown", "black", "white"],
 }
 
@@ -28,9 +32,43 @@ const samsunggalaxym53 = {
     title: "Samsung galaxy M53",
     specification: "Camera 30M, Ram 8GB, chipset up to date.",
     price: 3500000,
+    discount: true,
     color: ["brown", "black", "blue"],
 }
 
+// array of object
+
+const samsungGalaxy = [
+    {
+    title: "Samsung S22 Ultra",
+    specification: "Camera 50M, Ram 12GB, Fast Charger.",
+    price: 5000000,
+    discount: true,
+    color: ["white", "black", "blue"],
+
+    },
+    {
+    title: "Samsung galaxy M13",
+    specification: "Camera 20M, Ram 6GB, internal up to 1TB.",
+    price: 2000000,
+    discount: true,
+    color: ["white", "black", "blue"],
+    },
+    {
+    title: "Samsung galaxy M33",
+    specification: "Camera 30M, Ram 8GB, internal up to 1TB.",
+    price: 3000000,
+    discount: false,
+    color: ["brown", "black", "white"],
+    },
+    {
+    title: "Samsung galaxy M53",
+    specification: "Camera 30M, Ram 8GB, chipset up to date.",
+    price: 3500000,
+    discount: true,
+    color: ["brown", "black", "blue"],
+    },
+]
 
 // 2. Cek nilai minimal KKM dari 5 data siswa
 
