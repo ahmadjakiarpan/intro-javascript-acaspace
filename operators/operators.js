5a97a1027fb91dce7073d53af717cbfde3a04258
ahmadjakiarpan <ahmadjakii462@gmail.com>
2025-07-19 09:49:31 +0700

    add operators

---
+++ operators/operators.js
@@ -0,0 +1,58 @@
+// Arithmethic Operator
+
+let a = 10;
+let b = 3;
+
+// Penjumlahan
+console.log(a+b); // 13
+
+// Pengurangan
+console.log(a-b) // 7
+
+// Perkalian
+console.log(a*b) // 30
+
+// Pembagian
+console.log(a/b) // 3.333...
+
+// Modulus (Sisa Bagi)
+console.log(a%b) // 1
+
+// Eksponen (Pangkat)
+console.log(a**b) // 1000
+
+// Increment
+a++;
+console.log(a) //11
+
+// Decrement
+b--;
+console.log(b) // 2
+
+// Comparison Operator
+
+let passwordDaftar = "rahasia";
+let passwordIsi = "rahasia";
+
+// apakah passwordDaftar sama dengan passwordIsi?
+// output: true
+console.log(passwordDaftar == passwordIsi)
+
+// apakah passwordDaftar tidak sama dengan passwordIsi?
+// output: false
+console.log(passwordDaftar != passwordIsi)
+
+let umueNaekMotor = 17;
+let udin = 18;
+
+// apakah udin lebih dari atau sama dengan umurNaekMotor?
+console.log(udin > umueNaekMotor)
+
+// apakah udin kurang dar umurNaekMotor?
+console.log(udin<umueNaekMotor)
+
+// apakah udin lebih dari atau sama dengan umurNaekMotor?
+console.log(udin >= umueNaekMotor)
+
+// apakah udin kurang dari umurNaekMotor?
+console.log(udin<=umueNaekMotor)
