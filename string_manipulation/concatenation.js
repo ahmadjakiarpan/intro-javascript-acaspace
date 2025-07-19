078a4a594f7f6f086f6c1d604e5ab0acfe8cf09b
ahmadjakiarpan <ahmadjakii462@gmail.com>
2025-07-19 09:24:53 +0700

    string maniputalion

--- string_manipulation/concatenation.js
+++ string_manipulation/concatenation.js
@@ -13,3 +13,36 @@ const fullstackJavascript = {
 
 // Concatenation
 console.log("kita akan belajar" + " " + fullstackJavascript.title + " " + "dengan instruktur yang bernama" + " " + fullstackJavascript.instructorName)
+
+// Tamplate Literals
+// console.log('kita akan belajar ${fullstackJavascript.title} dengan instruktur yang bernama ${fullstackJavascript.instructorName}')
+
+const dataUntukEmail = {
+    namaProyek: "pertambangan",
+    namaPenerima: "Bos Tambang",
+    hari: "Sabtu",
+    tanggal: "13 Jul 2025",
+    tempat: "google Meet",
+    namaPengirim: "Udin",
+    jabatan: "Sekertaris",
+    kontak: "081234567879"
+}
+
+console.log(dataUntukEmail.namaProyek)
+
+const dataUntukEmailKedua = {
+    namaProyek: "Jalan Tol",
+    namaPenerima: "Client Tambang",
+    hari: "Sabtu",
+    tanggal: "13 Juli 2025",
+    tempat: "Google Meet",
+    namaPengirim: "Udin",
+    jabatan: "Sekertaris",
+    kontak: "08123456789"
+}
+
+
+// DRY : Don't Repeat Yourself
+console.log(`
+    Subjek: undangan Rapat Proyek ${dataUntukEmailKedua.namaProyek}
+    YTH ${dataUntukEmailKedua.namaPenerima}`)
