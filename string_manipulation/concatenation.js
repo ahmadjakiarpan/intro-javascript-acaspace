392d413429bf84c07ded02ad0f9fa48234633b05
ahmadjakiarpan <ahmadjakii462@gmail.com>
2025-07-19 08:57:58 +0700

    string maniputalion

---
+++ string_manipulation/concatenation.js
@@ -0,0 +1,15 @@
+const namaDepan = "Ramdlan"
+const namaBelakang = "Faqih"
+
+const fullstackJavascript = {
+    title: "Fullstack Javascript",
+    description: "Kursus Fullstack Javascript Intensif",
+    instructorName: "Ramdlan Faqih",
+    location: "Indonesia",
+    skill: ["HTML", "CSS", "Javascript", "React", "Node.js"],
+}
+
+// Kita akan belajar fullstackjavascript, dengan instruktur yang bernama Ramdlan Faqih asal dari Indonesia
+
+// Concatenation
+console.log("kita akan belajar" + " " + fullstackJavascript.title + " " + "dengan instruktur yang bernama" + " " + fullstackJavascript.instructorName)
