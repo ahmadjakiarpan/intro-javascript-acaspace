0ba0875c5d3d930d82b89fecc78ab9a042735847
ahmadjakiarpan <ahmadjakii462@gmail.com>
2025-07-24 19:44:48 +0700

    updated

--- looping/1.forloop.js
+++ looping/1.forloop.js
@@ -4,14 +4,14 @@
 // 3. counter (increment atau decrement)
 
 // for (let i = 3; i <=9; i++) {
-  //  console.log(i);
-    // apakah i kurang dari 9?
-    // kalau kurang i++ akan berjalan, lalu kode jalan lagi dari atas
+//  console.log(i);
+// apakah i kurang dari 9?
+// kalau kurang i++ akan berjalan, lalu kode jalan lagi dari atas
 //}
 
-const hargaBarang = [4000,1000]
+// const hargaBarang = [4000,1000]
 
-let total = 0
+// let total = 0
 // iterasi harga barang
 
 for (let i = 0; i < hargaBarang.length; i++) {
@@ -22,4 +22,12 @@ for (let i = 0; i < hargaBarang.length; i++) {
     }
 }
 
-//console.log("total harga keselurauhan", total)
+// console.log("total harga keseluruhan", total)
+// kondisi didalam sebuah looping
+for (let i = 1; i <= 9; i++) {
+    if (i % 2 === 0) {
+        console.log("ini adalah bilangan genap" + " " + i);
+    } else {
+        console.log("ini adalah bilangan ganjil" + " " + i);
+    }
+}
